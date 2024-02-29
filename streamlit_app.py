f1d1128596bd4a99c8810d2602c2b5f7b9d40ae5
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-02-29 09:18:56 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,38 +19,48 @@ def app():
     st.title('Logistic Regression, Naive Bayes Classifiers and Support Vector Machine')
     st.subheader('by Louie F. Cervantes M.Eng., WVSU College of ICT')
  
-    # Create a slider with a label and initial value
     if "n_samples" not in st.session_state:        
-        st.session_state['n_samples'] = st.slider(
-            label="Number of samples (200 to 4000):",
-            min_value=200,
-            max_value=4000,
-            step=200,
-            value=1000,  # Initial value
-            on_change=update_values(st.session_state['n_samples']), 
-            key="n_samples"
-        )
-  
-    if "random_state" not in st.session_state:        
-        st.session_state['random_state'] = st.slider(
-            label="Random seed (between 0 and 100):",
-            min_value=0,
-            max_value=100,
-            value=42,  # Initial value
-            on_change=update_values(st.session_state['random_state']), 
-            key="random_state"
-        )
-   
-    if "n_clusters" not in st.session_state:        
-        st.session_state['n_clusters'] = st.slider(
-            label="Number of Clusters:",
-            min_value=2,
-            max_value=6,
-            value=2,  # Initial value
-            on_change=update_values(st.session_state['n_clusters']), 
-            key="n_clusters"
+            st.session_state['n_samples'] = 0
 
-        )
+    if "random_state" not in st.session_state:        
+            st.session_state['random_state'] = 0
+
+
+    if "n_clusters" not in st.session_state:        
+            st.session_state['n_clusters'] = 0
+
+    # Create a slider with a label and initial value
+
+    
+    st.session_state['n_samples'] = st.slider(
+        label="Number of samples (200 to 4000):",
+        min_value=200,
+        max_value=4000,
+        step=200,
+        value=1000,  # Initial value
+        on_change=update_values(st.session_state['n_samples']), 
+        key="n_samples"
+    )
+
+   
+    st.session_state['random_state'] = st.slider(
+        label="Random seed (between 0 and 100):",
+        min_value=0,
+        max_value=100,
+        value=42,  # Initial value
+        on_change=update_values(st.session_state['random_state']), 
+        key="random_state"
+    )
+   
+
+    st.session_state['n_clusters'] = st.slider(
+        label="Number of Clusters:",
+        min_value=2,
+        max_value=6,
+        value=2,  # Initial value
+        on_change=update_values(st.session_state['n_clusters']), 
+        key="n_clusters"
+    )
 
     # Call the function once to display initial values
     # update_values()
